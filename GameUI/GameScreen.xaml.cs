938ecdfa139fd34d4ce59ae6ae711cf350fcf6a0
Patryk Gulczyński <w60059@student.wsiz.edu.pl>
2021-10-16 01:28:31 +0200

    Configured background to check for the look and feel of the window.

--- GameUI/GameScreen.xaml.cs
+++ GameUI/GameScreen.xaml.cs
@@ -10,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GameUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameScreen : Window
     {
-        public MainWindow()
+        public GameScreen()
         {
             InitializeComponent();
-
-            GameScreen gameScreen = new GameScreen();
-            gameScreen.Show();
-            Close();
         }
     }
 }
